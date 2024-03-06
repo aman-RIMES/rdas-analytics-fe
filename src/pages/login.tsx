6e1c1668ef92abec2974e2dc248a8bd95df8bd5e
aman-icap <atg2156@cumc.columbia.edu>
2024-03-06 16:11:39 +0300

    initial commit :)

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/components/login-form";
+
+const Login = () => {
+  return (
+    <>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <LoginForm />
+      </div>
+    </>
+  );
+};
+
+export default Login;
