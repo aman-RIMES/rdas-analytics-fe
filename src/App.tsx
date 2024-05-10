f84ceb40c0d50927630178f72ed06542ce3f00e6
aman-icap <atg2156@cumc.columbia.edu>
2024-05-10 08:06:08 +0300

    (chore) removing unnecessary components and code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import NotFoundPage from "./components/404-page";
-import Login from "./pages/login";
 import { ThemeProvider } from "./components/theme-provider";
 import Content from "./components/content";
-import AnalysisTopics from "./components/analysis-topics";
 function App() {
   const router = createBrowserRouter([
     {
@@ -17,40 +11,14 @@ function App() {
       errorElement: <NotFoundPage />,
       children: [
         {
-          path: "/content",
-          element: <Content />,
-        },
-        {
           path: "/analyze/:topic",
           element: <Content />,
         },
       ],
     },
     {
-      path: "/home",
-      element: <Navigate to="/" />,
-    },
-    {
       path: "/dashboard",
       element: <Home />,
-      children: [
-        {
-          path: "/dashboard/content",
-          element: <Content />,
-        },
-        {
-          path: "/dashboard/:topic",
-          element: <AnalysisTopics />,
-        },
-        {
-          path: "/dashboard/:topic/analyze",
-          element: <Content />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
     },
   ]);
 
