f25c811b2689797216edae718e87b1b937fd3151
aman-icap <atg2156@cumc.columbia.edu>
2024-08-13 16:17:24 +0300

    (wip) minor improvements

--- src/components/menu-button.tsx
+++ src/components/menu-button.tsx
@@ -1,14 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useEffect } from "react";
 
 export const MenuButton = ({ url, icon, title }: any) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location.pathname);
+  });
 
   return (
     <Button
       variant="ghost"
-      className="w-full justify-between hover:bg-yellow-300"
+      className={
+        cn(
+          location.pathname === "/predictive-tools/"
+            ? "bg-red-400"
+            : "bg-blue-200"
+        ) + "w-full justify-between hover:bg-yellow-300"
+      }
       onClick={() => navigate(url)}
     >
       <div className="flex items-center">
