ce873f6229baf37bfbe411f8010895d45793c735
amanryder <getahunamanuel@gmail.com>
2024-12-21 12:54:51 +0800

    (chore) update irframe url for LULC tool

--- src/components/analytics-tools/land-use.tsx
+++ src/components/analytics-tools/land-use.tsx
@@ -4,7 +4,7 @@ const LandUse = () => {
   return (
     <div className=" h-screen w-screen">
       <Iframe
-        url="https://care-lc.rimes.int/sasia/"
+        url="https://care-lc.rimes.int/sasia/?nav=0"
         width="100%"
         height="100%"
         // id=""
