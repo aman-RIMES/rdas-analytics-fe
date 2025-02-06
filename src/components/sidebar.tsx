12f3a62ba878fa706b24da7f8fc8864f648fcb1a
amanryder <getahunamanuel@gmail.com>
2025-02-06 13:28:43 +0800

    (fix) update sidebar navigation menu items for mobile view

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,74 +9,67 @@ export function Sidebar() {
       <div className="space-y-4 my-1 bg-green-800 rounded-lg p-5 h-screen shadow-md">
         <div className="pl-3 py-2 max-w-80 text-white">
           <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight ">
-            Climate
-          </h2>
-          <div className="space-y-1">
-            <MenuButton
-              url={"/climate-analytics"}
-              icon={
-                <Icons.climate_behaviors className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
-              }
-              title={"Climate Analytics"}
-            />
-            <MenuButton
-              url={"/climate-predictive-tools"}
-              icon={
-                <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
-              }
-              title={"Climate Predictive Tools"}
-            />
-            <MenuButton
-              url={"/gdd-predictive-tools"}
-              icon={
-                <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
-              }
-              title={"GDD Predictive Tool"}
-            />
-          </div>
-        </div>
-        <div className="pl-3 py-2 max-w-80 text-white">
-          <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
-            El Nino
+            Analytics
           </h2>
           <div className="space-y-1">
             <MenuButton
               url={"/elnino-analytics"}
               icon={
-                <Icons.climate_and_crops className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
+                <Icons.climate_behaviors className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
               }
-              title={"El Nino Analytics"}
+              title={"El Nino and Local Climate"}
             />
             <MenuButton
-              url={"/predictive-tools"}
+              url={"/lanina-analytics"}
               icon={
                 <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
               }
-              title={"El Nino Predictive Tools"}
+              title={"La Nina and Local Climate"}
+            />
+            <MenuButton
+              url={"/analytics-crop"}
+              icon={
+                <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
+              }
+              title={"Crop Calendar Suitability to Observed Climate"}
+            />
+            <MenuButton
+              url={"/analytics-land"}
+              icon={
+                <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
+              }
+              title={"Land Use & Land Cover Change"}
             />
           </div>
         </div>
-        {/* <div className="pl-3 py-2 max-w-80 text-white">
+        <div className="pl-3 py-2 max-w-80 text-white">
           <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
-            Transport
+            Predictive Tools
           </h2>
           <div className="space-y-1">
             <MenuButton
-              url={"/transport-analytics"}
+              url={"/predictive-tools"}
               icon={
-                <Icons.accidents className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
+                <Icons.climate_and_crops className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
               }
-              title={"Transport Analytics"}
+              title={"El Nino Impacts Prediction"}
             />
             <MenuButton
-              url={"/transport-predictive-tools"}
+              url={"/lanina-predictive-tools"}
               icon={
                 <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
               }
-              title={"Transport Predictive Tools"}
+              title={"La Nina Impacts Prediction"}
+            />
+            <MenuButton
+              url={"/predictive-temps"}
+              icon={
+                <Icons.report className="w-7 h-7 mr-2 bg-yellow-500 text-gray-800 rounded-sm p-1" />
+              }
+              title={"TempS"}
             />
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
